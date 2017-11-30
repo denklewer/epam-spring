6d134c26d51973dc7af0ea4907b418e25ada0357
denklewer <denklewer@gmail.com>
2017-12-01 01:04:36 +0300

    singletone with prototypes works but is not bat practice (scope->proxyMode)

--- src/main/java/screensaver/Config.java
+++ src/main/java/screensaver/Config.java
@@ -9,7 +9,7 @@ import java.util.Random;
 @ComponentScan(basePackages = "screensaver")
 public class Config {
     @Bean
-    @Scope("prototype")
+    @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS) //каждый раз получаем новый бин при обращении к color
     public Color color(){
     Random random = new Random();
     return  new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
